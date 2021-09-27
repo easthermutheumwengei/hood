e3a696d9120fa421372b3fec4028e921ce3f3c17
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-09-27 12:46:56 +0300

    add urls

---
+++ hood/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns =[
+    path('', views.index, name='index'),
+    path('feed/', views.feed, name='feed'),
+    path('businesses/', views.businesses, name='businesses'),
+    path('neigborhood/', views.neighborhood, name='neighborhood'),
+    path('profile/', views.profile, name='profile'),
+    path('auth/login/', views.login_user, name='login'),
+    path('auth/register/', views.register_user, name='register'),
+    path('accounts/logout/', views.logout_user, name='logout'),
